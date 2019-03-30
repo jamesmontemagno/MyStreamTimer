2913660687a9adec1a5753748d3854c9cb4257fa
James Montemagno <james.montemagno@gmail.com>
2019-03-30 12:51:29 -0700

    Single instance app

--- MyStreamTimer.WPF/App.xaml.cs
+++ MyStreamTimer.WPF/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using MyStreamTimer.Shared.Helpers;
@@ -15,9 +16,19 @@ namespace MyStreamTimer.WPF
     /// </summary>
     public partial class App : Application
     {
-
+        static Mutex mutex = null;
         protected override void OnStartup(StartupEventArgs e)
         {
+            const string appName = "My Stream Timer";
+            bool createdNew;
+
+            mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application  
+                Application.Current.Shutdown();
+            }
             base.OnStartup(e);
             ServiceContainer.Register<IClipboard>(() => new ClipboardImplementation());
         }
