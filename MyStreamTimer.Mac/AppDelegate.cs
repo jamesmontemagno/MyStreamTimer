7b5ca7038749950602871ea486675a4969ae120c
James Montemagno <james.montemagno@gmail.com>
2019-04-20 22:27:10 -0700

    First try at custom urls

--- MyStreamTimer.Mac/AppDelegate.cs
+++ MyStreamTimer.Mac/AppDelegate.cs
@@ -30,7 +30,33 @@ namespace MyStreamTimer.Mac
 
             ServiceContainer.Register<IClipboard>(() => new ClipboardImplementation());
             LoadApplication(new MyStreamTimer.UI.App());
+           
+            
+            var appleEventManager = NSAppleEventManager.SharedAppleEventManager;
+
+            appleEventManager.SetEventHandler(this, new ObjCRuntime.Selector("handleGetURLEvent:withReplyEvent:"), AEEventClass.Internet, AEEventID.GetUrl);
+
             base.DidFinishLaunching(notification);
         }
+
+        [Export("handleGetURLEvent:withReplyEvent:")]
+        private void HandleGetURLEvent(NSAppleEventDescriptor descriptor, NSAppleEventDescriptor replyEvent)
+        {
+            // Breakpoint here, debug normally and *then* call your URL
+            var keyDirectObject = "----";
+            var keyword = (((uint)keyDirectObject[0]) << 24 |
+                           ((uint)keyDirectObject[1]) << 16 |
+                           ((uint)keyDirectObject[2]) << 8 |
+                           ((uint)keyDirectObject[3]));
+            var urlString = descriptor.ParamDescriptorForKeyword(keyword).StringValue;
+        }
+
+        public override void OpenUrls(NSApplication application, NSUrl[] urls)
+        {
+            
+            base.OpenUrls(application, urls);
+        }
+
+
     }
 }
---
+++ MyStreamTimer.Shared/Helpers/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace MyStreamTimer.Shared.Helpers
+{
+    public static class Utils
+    {
+        public static (bool, int) ParseStartupArgs(string args)
+        {
+            var start = false;
+            var mins = -1;
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(args))
+                {
+                    var uri = new Uri(args);
+                    if (uri.Host.ToLower() == "countdown" && uri.Query.ToLower().Contains("?mins=") && int.TryParse(uri.Query.Remove(0, 6), out mins))
+                    {
+                        start = true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return (start, mins);
+        }
+    }
+}
