be40c2c3aa321e312d6d010e5690cea872668a1b
James Montemagno <james.montemagno@gmail.com>
2021-09-18 17:13:30 -0700

    Add beeps on UWP

--- MyStreamTimer.UWP/Services/PlatformHelpers.cs
+++ MyStreamTimer.UWP/Services/PlatformHelpers.cs
@@ -6,6 +6,7 @@ using Xamarin.Essentials;
 using Windows.Services.Store;
 using System.Threading;
 using Windows.Foundation;
+using Windows.Storage.Streams;
 
 namespace MyStreamTimer.UWP.Services
 {
@@ -41,6 +42,62 @@ namespace MyStreamTimer.UWP.Services
         {
 
         }
+
+        public async Task Beep()
+        {
+            await Xamarin.Essentials.MainThread.InvokeOnMainThreadAsync(async () =>
+            {
+                try
+                {
+                
+
+                    var mediaElement1 = new Windows.UI.Xaml.Controls.MediaElement();
+                    var beepStream = await BeepBeep(200, 2000, 75);
+                    mediaElement1.SetSource(beepStream, string.Empty);
+                    mediaElement1.Play();
+                    await Task.Delay(200);
+                    mediaElement1.Play();
+                    await Task.Delay(200);
+                    mediaElement1.Play();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            });
+        }
+
+        public async Task<IRandomAccessStream> BeepBeep(int Amplitude, int Frequency, int Duration)
+        {
+            var A = ((Amplitude * (System.Math.Pow(2, 15))) / 1000) - 1;
+            var DeltaFT = 2 * Math.PI * Frequency / 44100.0;
+
+            var Samples = 441 * Duration / 10;
+            var Bytes = Samples * 4;
+            int[] Hdr = { 0X46464952, 36 + Bytes, 0X45564157, 0X20746D66, 16, 0X20001, 44100, 176400, 0X100004, 0X61746164, Bytes };
+
+            var ims = new InMemoryRandomAccessStream();
+            var outStream = ims.GetOutputStreamAt(0);
+            var dw = new DataWriter(outStream);
+            dw.ByteOrder = ByteOrder.LittleEndian;
+
+            for (var I = 0; I < Hdr.Length; I++)
+            {
+                dw.WriteInt32(Hdr[I]);
+            }
+            for (var T = 0; T < Samples; T++)
+            {
+                var Sample = System.Convert.ToInt16(A * Math.Sin(DeltaFT * T));
+                dw.WriteInt16(Sample);
+                dw.WriteInt16(Sample);
+            }
+            await dw.StoreAsync();
+            await outStream.FlushAsync();
+            return ims;
+        }
+
+        public void StartActivity(string id) { }
+        public void StopActivity(string id) { }
     }
 
     static partial class PlatformExtensions
