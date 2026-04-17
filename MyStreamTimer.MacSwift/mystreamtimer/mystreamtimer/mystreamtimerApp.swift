08a855411241014677d035921fa51b884b77d11c
James Montemagno <jame.smontemagno>
2026-04-16 23:40:07 -0700

    Add initial project files and structure for MyStreamTimer app

---
+++ MyStreamTimer.MacSwift/mystreamtimer/mystreamtimer/mystreamtimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mystreamtimerApp.swift
+//  mystreamtimer
+//
+//  Created by James Montemagno on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct mystreamtimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
